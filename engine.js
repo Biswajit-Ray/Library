33eb34bce29685f5f108e6ee46d27b5772446e12
Biswajit Ray <220484600+Biswajit-Ray@users.noreply.github.com>
2026-04-23 14:46:03 +0000

    Complete the project

--- engine.js
+++ engine.js
@@ -1,8 +1,6 @@
-const { createElement } = require("react");
-
 console.log("Working Properly!");
 
-const myLibrary = [
+let myLibrary = [
     { title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180, readStatus: false, id: 69420 },
     { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281, readStatus: false, id: 69420 },
     { title: "1984", author: "George Orwell", pages: 328, readStatus: false, id: 69420 },
@@ -20,10 +18,19 @@ const myLibrary = [
     { title: "Neuromancer", author: "William Gibson", pages: 271, readStatus: false, id: 69420 }
 ];
 
+myLibrary= myLibrary.map((book)=>{
+    return {
+        ...book,
+        id: crypto.randomUUID()
+    }
+});
+
+console.log(myLibrary);
+
 const libraryGrid = document.querySelector(".library-grid");
 
 
-function Book(title, author, pages, id){
+function Book(title, author, pages, id){ //Factory function to make a "Book"
     this.title= title;
     this.author= author;
     this.pages= pages;
@@ -31,8 +38,12 @@ function Book(title, author, pages, id){
     this.id= id;
 }
 
-function Logger(title, author, pages){
-    let uniqueID= 69420;
+Book.prototype.toggleRead= function() {
+    this.readStatus= !this.readStatus
+}
+
+function Logger(title, author, pages){ // Function to make a book and add it to the Array [myLibrary]
+    let uniqueID= crypto.randomUUID();
     console.log(uniqueID);
     let book = new Book(title, author, pages, uniqueID);
     console.log(book);
@@ -40,41 +51,84 @@ function Logger(title, author, pages){
     console.log(myLibrary[myLibrary.length-1]);
 }
 
-function cardMaker(this){
+function removeBook(id){ //removes book with a generated custom id from both DOM and DB [To be used as a callback function]
+    
+    //Removes from array
+    myLibrary= myLibrary.filter(book=> book.id != id);
+
+    //Removes from DOM 
+    const cardToRemove = document.querySelector(`[data-id="${id}"]`).closest(".card");
+    cardToRemove.remove();
+}
+
+function cardMaker(target){ // Makes a card for display at home page. [DOM manipulation]_______input is the object stored in [myLibray]
     const card= document.createElement("div");
     card.classList.add("card");
+    card.setAttribute("data-id", target.id);
 
     const author= document.createElement("p");
-    author.classList.add(".author");
-    author.innerText= `${this.author}`;
+    author.classList.add("author");
+    author.innerText= `${target.author}`;
 
     const title= document.createElement("p");
-    title.classList.add(".title");
-    title.innerText= `${this.title}`;
+    title.classList.add("title");
+    title.innerText= `${target.title}`;
 
-    const read_Status= document.createElement("p");
-    read_Status.classList.add(".readStatus");
-    if(this.readStatus===true) {
-        read_Status.innerText= "Read";
-        read_Status.classList.add(".read-true");        
-    } else {
-        read_Status.innerText= "Not Read";
-        read_Status.classList.add(".read-false");
-    }
+    const read_Status= document.createElement("button");
+    read_Status.classList.add("readStatus");
+    read_Status.innerText= `Not Read`;
+    read_Status.classList.add("read-false");
+
+    read_Status.addEventListener("click", (e)=>{//button function to change class from "read" to "unread"
+        if(target.readStatus===true) {
+            read_Status.innerText= "Read";
+            read_Status.classList.add("read-true");
+            read_Status.classList.remove("read-false");
+            target.readStatus= false;
+        } else {
+            read_Status.innerText= "Not Read";
+            read_Status.classList.remove("read-true");
+            read_Status.classList.add("read-false");
+            target.readStatus= true;
+        }
+    });
 
     const pages = document.createElement("p");
-    pages.classList.add(".pages");
-    pages.innerText= `${this.pages}`
+    pages.classList.add("pages");
+    pages.innerText= `${target.pages}`
 
     const lowBar = document.createElement("div");
-    lowBar.classList.add(".low-bar");
+    lowBar.classList.add("low-bar");
+
+    const rmvbtn = document.createElement("button");
+    rmvbtn.classList.add("remove");
+    rmvbtn.innerText= "remove";
+    rmvbtn.addEventListener("click", (e)=>{removeBook(target.id)});
+
 
     lowBar.append(pages, read_Status);
-    card.append(title, author, lowBar);
+    card.append(title, author, lowBar, rmvbtn);
+    libraryGrid.appendChild(card)
 }
 
 function displayer(){
-    for(let item of myLibrary ){
+    for(item of myLibrary){
         console.log(item.author);
+        cardMaker(item);
     }
 }
+
+const submitButton= document.getElementById("submit");
+submitButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const __author= document.getElementById("author");
+    const __title = document.getElementById("title");
+    const __pages = document.getElementById("pages");
+    console.log(__title.value)
+    Logger(__title.value, __author.value, __pages.value);
+    __title.value="";
+    __author.value= "";
+    __pages.value= "";
+})
+
+displayer();
